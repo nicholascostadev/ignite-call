a4d2a4d559ae05728d1993a7ea70175a81babc0d
Nicholas Costa <nicholascostadev@gmail.com>
2022-12-27 18:39:44 -0300

    refactor: update get-week-days to support short weekDay

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,7 +1,17 @@
-export const getWeekDays = () => {
+type GetWeekDaysParams = {
+  short?: boolean
+}
+
+export const getWeekDays = ({ short = false }: GetWeekDaysParams = {}) => {
   const { format } = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
     .map((day) => format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) => weekDay.charAt(0).toUpperCase() + weekDay.slice(1))
+    .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
+      return weekDay.charAt(0).toUpperCase() + weekDay.slice(1)
+    })
 }
